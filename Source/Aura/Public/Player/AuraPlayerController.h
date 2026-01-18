83bff7024a5f78b7427e320d96c23af26f6838b4
STARRY-COMPLEX <211924956@qq.com>
2026-01-18 21:47:05 +0800

    Aura Player Controller

--- Source/Aura/Public/Player/AuraPlayerController.h
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -6,9 +6,22 @@
 #include "GameFramework/PlayerController.h"
 #include "AuraPlayerController.generated.h"
 
+class UInputMappingContext;
+
 UCLASS()
 class AURA_API AAuraPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+	
+public:
+	AAuraPlayerController();
+	
+protected:
+	virtual void BeginPlay() override;
+	
+private:
+	UPROPERTY(EditAnywhere, Category = "Custom Property | Input")
+	TObjectPtr<UInputMappingContext> AuraContext;
+	
 	
 };
