4011ff4d450c9097f113375e31ff23ce1ea88476
STARRY-COMPLEX <211924956@qq.com>
2026-01-29 16:41:25 +0800

    Overlay Widget Controller

---
+++ Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
@@ -0,0 +1,5 @@
+// Copyright Lie
+
+
+#include "UI/WidgetController/OverlayWidgetController.h"
+
--- Source/Aura/Public/UI/HUD/AuraHUD.h
+++ Source/Aura/Public/UI/HUD/AuraHUD.h
@@ -6,20 +6,35 @@
 #include "GameFramework/HUD.h"
 #include "AuraHUD.generated.h"
 
+class UAttributeSet;
+class UAbilitySystemComponent;
+class UOverlayWidgetController;
 class UAuraUserWidget;
+struct FWidgetControllerParams;
 
 UCLASS()
 class AURA_API AAuraHUD : public AHUD
 {
 	GENERATED_BODY()
 public:
+	AAuraHUD();
+	
 	UPROPERTY()
 	TObjectPtr<UAuraUserWidget> OverlayWidget;
 	
+	UOverlayWidgetController* GetOverlayWidgetController(const FWidgetControllerParams& WCParams);
+	
+	void InitOverlay(APlayerController* PC, APlayerState* PS, UAbilitySystemComponent* ASC, UAttributeSet* AS);
+	
 protected:
-	virtual void BeginPlay() override;
 	
 private:
 	UPROPERTY(EditAnywhere, Category = "Custom Property")
 	TSubclassOf<UAuraUserWidget> OverlayWidgetClass;
+	
+	UPROPERTY()
+	TObjectPtr<UOverlayWidgetController> OverlayWidgetController;
+	
+	UPROPERTY(EditAnywhere, Category = "Custom Property")
+	TSubclassOf<UOverlayWidgetController> OverlayWidgetControllerClass;
 };
