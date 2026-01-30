8e10f09f09cbc2132355fc13c41fd46bc8f7e776
STARRY-COMPLEX <211924956@qq.com>
2026-01-30 16:45:25 +0800

    Callbacks for Mana Changes

--- Source/Aura/Public/UI/WidgetController/OverlayWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/OverlayWidgetController.h
@@ -10,6 +10,8 @@ struct FOnAttributeChangeData;
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnHealthChangedSignature, float, NewHealth);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMaxHealthChangedSignature, float, NewMaxHealth);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnManaChangedSignature, float, NewMana);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMaxManaChangedSignature, float, NewMaxMana);
 
 UCLASS(BlueprintType, Blueprintable)
 class AURA_API UOverlayWidgetController : public UAuraWidgetController
@@ -26,7 +28,14 @@ public:
 	UPROPERTY(BlueprintAssignable, Category = "Custom Property | GAS | Attributes")
 	FOnMaxHealthChangedSignature OnMaxHealthChanged;
 	
+	UPROPERTY(BlueprintAssignable, Category = "Custom Property | GAS | Attributes")
+	FOnManaChangedSignature OnManaChanged;
+	
+	UPROPERTY(BlueprintAssignable, Category = "Custom Property | GAS | Attributes")
+	FOnMaxManaChangedSignature OnMaxManaChanged;
 protected:
 	void HealthChanged(const FOnAttributeChangeData& Data) const;
 	void MaxHealthChanged(const FOnAttributeChangeData& Data) const;
+	void ManaChanged(const FOnAttributeChangeData& Data) const;
+	void MaxManaChanged(const FOnAttributeChangeData& Data) const;
 };
