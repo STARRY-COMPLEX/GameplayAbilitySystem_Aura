bc6e6ad86c0526c1cf8996d999a9d573dce56e07
STARRY-COMPLEX <211924956@qq.com>
2026-02-13 15:34:47 +0800

    Get All Asset Tags

--- Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
@@ -9,5 +9,11 @@ void UAuraAbilitySystemComponent::AbilityActorInfoSet(){
 
 void UAuraAbilitySystemComponent::EffectApplied(UAbilitySystemComponent* AbilitySystemComponent,
                                                 const FGameplayEffectSpec& EffectSpec, FActiveGameplayEffectHandle ActiveEffectHandle){
-	GEngine->AddOnScreenDebugMessage(1, 8.f, FColor::Blue, FString("Effect Apply!"));
+	FGameplayTagContainer TagContainer;
+	EffectSpec.GetAllAssetTags(TagContainer);
+	for(const FGameplayTag& Tag : TagContainer){
+		// TODO: Broadcast the tag to the Widget Controller
+		const FString Message = FString::Printf(TEXT("GE Tag: %s"), *Tag.ToString());
+		GEngine->AddOnScreenDebugMessage(-1, 8.f, FColor::Blue, Message);
+	}
 }
