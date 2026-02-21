6a2790d15aa803ef1766002c70188127b210ce68
STARRY-COMPLEX <211924956@qq.com>
2026-02-21 14:50:30 +0800

    Derived Attributes

--- Source/Aura/Private/Character/Player/AuraCharacter.cpp
+++ Source/Aura/Private/Character/Player/AuraCharacter.cpp
@@ -64,5 +64,5 @@ void AAuraCharacter::InitAbilityActorInfo(){
 			AuraHUD->InitOverlay(AuraPlayerController, AuraPlayerState, AbilitySystemComponent, AttributeSet);
 		}
 	}
-	InitializePrimaryAttributes();
+	InitializeDefaultAttributes();	
 }
