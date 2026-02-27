afff702fa50184224e6e73a6226bbc7fa5f6b0c0
STARRY-COMPLEX <211924956@qq.com>
2026-02-27 13:57:23 +0800

    Attribute Menu Widget Controller

---
+++ Source/Aura/Private/UI/WidgetController/AuraMenuWidgetController.cpp
@@ -0,0 +1,12 @@
+// Copyright Lie
+
+
+#include "UI/WidgetController/AuraMenuWidgetController.h"
+
+void UAuraMenuWidgetController::BroadcastInitialValues(){
+	
+}
+
+void UAuraMenuWidgetController::BindCallbacksToDependences(){
+	
+}
