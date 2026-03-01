74e006b50b43e80b6eec0e5e5a969fbff122a1c3
STARRY-COMPLEX <211924956@qq.com>
2026-03-01 21:11:44 +0800

    Mapping Tags to Attributes

--- Source/Aura/Private/UI/WidgetController/AuraMenuWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/AuraMenuWidgetController.cpp
@@ -10,10 +10,12 @@ void UAuraMenuWidgetController::BroadcastInitialValues(){
 	UAuraAttributeSet* AS = CastChecked<UAuraAttributeSet>(AttributeSet);
 	
 	check(AttributeInfo);
-	
-	FAuraAttributeInfo Info = AttributeInfo->FindAttributeInfoForTag(FAuraGameplayTags::Get().Attributes_Primary_Strength);
-	Info.AttributeValue = AS->GetStrength();
-	AttributeInfoDelegate.Broadcast(Info);
+
+	for (TPair<FGameplayTag, FGameplayAttribute(*)()>& Pair : AS->TagsToAttributes){
+		FAuraAttributeInfo Info = AttributeInfo->FindAttributeInfoForTag(Pair.Key);
+		Info.AttributeValue = Pair.Value().GetNumericValue(AS);
+		AttributeInfoDelegate.Broadcast(Info);		
+	}
 }
 
 void UAuraMenuWidgetController::BindCallbacksToDependences(){
