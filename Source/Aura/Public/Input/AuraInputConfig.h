42dcf42ddc5de8e8cf2723a44565f5a95d8281c2
STARRY-COMPLEX <211924956@qq.com>
2026-03-07 17:59:37 +0800

    Input Config Data Asset

---
+++ Source/Aura/Public/Input/AuraInputConfig.h
@@ -0,0 +1,31 @@
+// Copyright Lie
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Engine/DataAsset.h"
+#include "AuraInputConfig.generated.h"
+
+USTRUCT(BlueprintType)
+struct FAuraInputAction{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditDefaultsOnly)
+	const class UInputAction* InputAction = nullptr;
+	
+	UPROPERTY(EditDefaultsOnly)
+	FGameplayTag InputTag = FGameplayTag();
+};
+
+UCLASS()
+class AURA_API UAuraInputConfig : public UDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+	const UInputAction* FindAbilityInputActionForTag(const FGameplayTag& InputTag, bool bLogNotFound = false) const;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Custom Property | Ability Input Action")
+	TArray<FAuraInputAction> AbilityInputActions;
+};
