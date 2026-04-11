9b965da27c51f070961536df730b41478530e591
STARRY-COMPLEX <211924956@qq.com>
2026-04-11 16:45:03 +0800

    Custom Gameplay Effect Context

---
+++ Source/Aura/Public/AuraAbilityTypes.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+#include "GameplayEffectTypes.h"
+#include "AuraAbilityTypes.generated.h"
+
+USTRUCT(BlueprintType)
+struct FAuraGameplayEffectContext : public FGameplayEffectContext{
+	GENERATED_BODY()
+	
+public:
+	bool IsCriticalHit() const {return bIsCriticalHit;}
+	bool IsBlockHit() const {return bIsBlockHit;}
+	
+	void SetIsCriticalHit(const bool bInIsCriticalHit){bIsCriticalHit = bInIsCriticalHit;}
+	void SetIsBlockHit(const bool bInIsBlockHit){bIsBlockHit = bInIsBlockHit;}
+	
+	virtual UScriptStruct* GetScriptStruct() const override
+	{
+		return FGameplayEffectContext::StaticStruct();
+	}
+	
+	virtual bool NetSerialize(FArchive& Ar, class UPackageMap* Map, bool& bOutSuccess) override;
+protected:
+	UPROPERTY()
+	bool bIsBlockHit = false;
+	
+	UPROPERTY()
+	bool bIsCriticalHit = false;
+};
