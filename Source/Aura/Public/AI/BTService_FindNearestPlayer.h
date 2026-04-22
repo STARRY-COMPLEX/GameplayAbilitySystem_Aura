36c35f603f1d2ee3198bc7c35c323f5e8e1ee387
STARRY-COMPLEX <211924956@qq.com>
2026-04-22 14:16:30 +0800

    Behavior Tree Service

---
+++ Source/Aura/Public/AI/BTService_FindNearestPlayer.h
@@ -0,0 +1,18 @@
+// Copyright Lie
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Services/BTService_BlueprintBase.h"
+#include "BTService_FindNearestPlayer.generated.h"
+
+UCLASS()
+class AURA_API UBTService_FindNearestPlayer : public UBTService_BlueprintBase
+{
+	GENERATED_BODY()
+	
+public:
+	
+protected:
+	virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+};
