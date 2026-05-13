73bc2033a819e75af138fb8ed75f776616a50f1f
STARRY-COMPLEX <211924956@qq.com>
2026-05-13 17:05:52 +0800

    Elementalist Attack Task

--- Source/Aura/Public/Character/AuraCharacterBase.h
+++ Source/Aura/Public/Character/AuraCharacterBase.h
@@ -34,6 +34,7 @@ public:
 	virtual UNiagaraSystem* GetBloodEffect_Implementation() override;
 	virtual FTagMontage GetTagMontageByTag_Implementation(const FGameplayTag& MontageTag) override;
 	virtual int32 GetMinionCount_Implementation() override;
+	virtual void IncrementMinionCount_Implementation(int32 Amount) override;
 	/** end Combat Interface */
 	
 	UFUNCTION(NetMulticast, Reliable)
