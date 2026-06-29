d40c7f1ac9b0173c8549ff937eec16dd24b959e0
STARRY-COMPLEX <211924956@qq.com>
2026-06-29 12:11:30 +0800

    Aura Beam Spell

---
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraBeamSpell.h
@@ -0,0 +1,29 @@
+// Copyright Lie
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/AuraDamageGameplayAbility.h"
+#include "AuraBeamSpell.generated.h"
+
+UCLASS()
+class AURA_API UAuraBeamSpell : public UAuraDamageGameplayAbility
+{
+	GENERATED_BODY()
+	
+public:
+	UFUNCTION(BlueprintCallable)
+	void StoreMouseDataInfo(const FHitResult& HitResult);
+	
+	UFUNCTION(BlueprintCallable)
+	void StoreOwnerPlayerController();
+protected:
+	UPROPERTY(BlueprintReadWrite, Category = "Custom Property | Beam")
+	FVector MouseHitLocation;
+	
+	UPROPERTY(BlueprintReadWrite, Category = "Custom Property | Beam")
+	TObjectPtr<AActor> MouseHitActor;
+	
+	UPROPERTY(BlueprintReadWrite, Category = "Custom Property | Beam")
+	TObjectPtr<APlayerController> OwnerPlayerController;
+};
