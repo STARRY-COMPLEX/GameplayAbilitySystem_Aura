2e29b8357af0e535ffb3620a7955480e8bdc3ed4
STARRY-COMPLEX <211924956@qq.com>
2026-06-29 22:43:37 +0800

    Gameplay Cue Notify Actor

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -134,6 +134,10 @@ FOnDeath& AAuraCharacterBase::GetOnDeathDelegate(){
 	return OnDeath;
 }
 
+USkeletalMeshComponent* AAuraCharacterBase::GetWeapon_Implementation(){
+	return Weapon;
+}
+
 
 void AAuraCharacterBase::InitAbilityActorInfo(){
 	
